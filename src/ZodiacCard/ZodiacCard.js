7b8731cf4924a663ab9ba4bb30b0ab8f8e37ff68
Beau Elliott <belliott1516@gmail.com>
2022-04-25 14:38:58 -0700

    got zodiac cards to render but images still not loading

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,11 +1,11 @@
 import './ZodiacCard.css';
 
-export default function ZodiacCard({ zodiac }) {
+export default function ZodiacCard({ name, dates }) {
   {/* in this component,  use the `name` and `dates` props to render the name and 
 dates of the sign, as well as load the correct image  from the public directory */}
   return <div className='zodiac-card'>
-    zodiac.name
-    <img src={`${zodiac.name}.png`}/>
-    zodiac.dates
+    <img src={`${name}.png`}/>
+    <h2>{ name }</h2>
+    <h4>{ dates }</h4>
   </div>;
 }
